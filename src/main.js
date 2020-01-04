87c53103270b1e3f54484daf2e8670495ea7fdb0
zhangzhongquan <shenkesinian@126.com>
2020-01-04 11:50:30 +0800

    '初始化基础版项目'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
